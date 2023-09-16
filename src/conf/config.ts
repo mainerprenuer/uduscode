6d46a8794c08e88ef1bd43f68aad4dcb958c04e2
mainerprenuer <moustymainer@gmail.com>
2023-09-16 20:20:05 +0100

    setting up appwrite config for handling logic data

---
+++ src/conf/config.ts
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteUrl: String(process.env.
+    NEXT_PUBLIC_APP_WRITE_URL),
+    appwriteProjectId: String(process.env.
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+}
+
+export default conf
